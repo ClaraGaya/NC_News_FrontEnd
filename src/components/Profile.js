4b75559760f2ec8daebc2a0f0fba55bf1cc8673d
jonsey247 <jacobfilmjones@hotmail.com>
2017-08-03 21:21:23 +0100

    Profile added

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Profile extends React.Component {
+    render() {
+        return (
+            <div className="column box profile">
+                Hello!!!<br />
+                Search Bar
+       </div>
+        );
+    }
+}
+
+export default Profile; 
