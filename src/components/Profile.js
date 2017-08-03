8fe20a52e5c24a7ecc678df44fba2310ca4db9d6
jonsey247 <jacobfilmjones@hotmail.com>
2017-08-03 21:31:07 +0100

    [Profile] displays avatar and username

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,9 +4,14 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="column box profile">
-                Hello!!!<br />
-                Search Bar
-       </div>
+                <div className="profileWrap">
+                    <img className="avatar" src="https://avatars3.githubusercontent.com/u/6791502?v=3&s=200" />
+                    <div className="profileName">
+                        <h2 className="title is-4">User_Name</h2>
+                    </div>
+                </div>
+                .<br />
+            </div>
         );
     }
 }
