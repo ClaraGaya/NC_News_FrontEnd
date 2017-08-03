d23469f5e70e7dfd18beaa242d219cb2ed2b14bb
jonsey247 <jacobfilmjones@hotmail.com>
2017-08-03 16:58:57 +0100

    [Comment] displays time and date correctly

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -3,19 +3,32 @@ import PropTypes from 'prop-types';
 
 class Comment extends React.Component {
     render() {
+        const timestamp = Number(this.props.comment.created_at);
         return (
-            <div>
-                <h2>Comment by {this.props.comment.created_by}</h2>
-                <p>{this.props.comment.created_at}</p>
-                <p>{this.props.comment.votes}</p>
-                <p>{this.props.comment.body}</p>
+            <div className="postedComment">
+                <h2>{this.props.comment.created_by} says:</h2>
+                <span className="commentBody">{this.props.comment.body}</span><br />
+                <span className="commentInfo">Posted at {timeConverter(timestamp)}<br />
+                    Votes: {this.props.comment.votes}</span>
             </div>
         );
     }
 }
 
-export default Comment;
+function timeConverter(UNIX_timestamp) {
+    var a = new Date(UNIX_timestamp);
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min;
+    return time;
+}
 
 Comment.proptypes = {
     comment: PropTypes.object.isRequired
-};  
+}; 
+ 
+export default Comment;
